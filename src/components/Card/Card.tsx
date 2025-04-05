5b03d528d5c74a9ae47120b7192df2b710bf7f89
Gastón Dávila <gastonn520@gmail.com>
2025-04-05 16:29:40 -0300

    arreglo detalle en envio

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import './Card.sass'
 const Card = ({
     instrumentoImagen = '',
@@ -6,21 +7,30 @@ const Card = ({
     instrumentoEnvio = '',
     instrumentoVendidos = ''
 }) => {
-
     return (
         <div className='card-container p-5 mt-3'>
             <div className='card-image mt-3'>
-            <img
-                className='card-image'
-                src={`./images/${instrumentoImagen}`}
-                alt={instrumentoImagen} 
-            />
+                <img
+                    className='card-image'
+                    src={`./images/${instrumentoImagen}`}
+                    alt={instrumentoImagen}
+                />
             </div>
             <div className='card-info'>
                 <h1 className='card-titulo m-0'>{instrumentoTitulo}</h1>
                 <p className='card-precio m-0'>${instrumentoPrecio}</p>
-                <p className='card-envio m-0 ms-2'>Costo de Envio Interior de Argentina: ${instrumentoEnvio}</p>
-                <p className='card-vendidos m-0'>{instrumentoVendidos} vendidos</p>
+                {instrumentoEnvio == 'G' ? (
+                    <div className='envio-gratis'>
+                        <img src="./images/camion.png" alt="logo-envio-gratis" />
+                        <p>Envío gratis a todo el país</p>
+                    </div>
+                ) : (
+                    <p className='card-envio m-0 ms-2'>Costo de Envio Interior de Argentina: ${instrumentoEnvio}</p>
+                )
+                }
+                <p className='card-vendidos'>{instrumentoVendidos} vendidos</p>
+
+                
             </div>
         </div>
     )
